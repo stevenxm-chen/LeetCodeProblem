3e42ed74d118f96e9244fd1c7dddaa75d5b485ad
Ximing Chen <steven.xm824@gmail.com>
2022-01-12 13:35:22 -0800

    native solution

--- 3Sum.js
+++ 3Sum.js
@@ -23,6 +23,43 @@
 // 0 <= nums.length <= 3000
 // -105 <= nums[i] <= 105
 
-var threeSum = function(nums) {
+var threeSum = function (nums) {
+  const result = [];
 
+  if (nums.length < 3) {
+    return result;
+  }
+
+  //iterate over nums, create sum in the loop
+  const past = {}
+  const hash = {};
+  let first = 0;
+
+  while (first < nums.length) {
+    const storage = {};
+     let sum = -nums[first];
+
+     //if duplitcate sum just no need to run again
+     if (past[-sum] !== undefined) {
+       first ++
+       continue;
+     }
+
+    for (i = first + 1; i < nums.length; i++) {
+      if (storage[sum - nums[i]] !== undefined) {
+        let combo = [-sum, sum - nums[i], nums[i]].sort();
+        if(hash[combo] === undefined) {
+          result.push(combo);
+          hash[combo] = true;
+        }
+
+      }
+      storage[nums[i]] = i;
+    }
+    past[-sum] = 1
+    first ++
+  }
+
+
+  return result;
 };
