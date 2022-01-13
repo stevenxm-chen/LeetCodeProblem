fb7e38a0f633aee5cdee0d2cf28994e4d19f3cac
Ximing Chen <steven.xm824@gmail.com>
2022-01-13 11:07:19 -0800

    simple solution

--- threeSumClosest.js
+++ threeSumClosest.js
@@ -23,6 +23,41 @@
 // -1000 <= nums[i] <= 1000
 // -104 <= target <= 104
 
-var threeSumClosest = function(nums, target) {
+var threeSumClosest = function (nums, target) {
 
+  let sum = 0;
+  let diff = 1000;
+  const storage = {};
+
+  // sort the array
+  nums = nums.sort((a, b) => a - b);
+
+  //iterate over nums i is first num, j 2nd. k 3rd
+  for (let i = 0; i < nums.length; i++) {
+
+    let j = i + 1;
+    let k = nums.length - 1;
+
+    debugger
+
+    while (j < k) {
+      sum = nums[i] + nums[j] + nums[k];
+      if (sum === target) {
+        return sum;
+      } else if (sum > target) {
+        if (sum - target < diff) {
+          diff = sum - target;
+          storage[diff] = sum;
+        }
+        k--;
+      } else {
+        if (target - sum < diff) {
+          diff = target - sum;
+          storage[diff] = sum;
+        }
+        j++;
+      }
+    }
+  }
+    return storage[diff];
 };
