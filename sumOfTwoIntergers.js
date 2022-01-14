b50ea1b59e82cfdae6f174cc4fa9ddcf347f5fbb
Ximing Chen <steven.xm824@gmail.com>
2022-01-14 12:01:54 -0800

    using array to solve

--- sumOfTwoIntergers.js
+++ sumOfTwoIntergers.js
@@ -22,6 +22,43 @@
  * @return {number}
  */
 
- var getSum = function(a, b) {
-  //
+var getSum = function(a, b) {
+  const arr = [];
+  let isNev = false;
+if ((a < 0 && b < 0) || (Math.abs(b) > Math.abs(a) && b < 0) ||  (Math.abs(a) > Math.abs(b) && a < 0)) {
+ isNev = true;
+}
+if(a * b >= 0) {
+ for (let i = 0; i < Math.abs(a); i++) {
+   arr.push(0);
+ }
+
+ for (let j = 0; j < Math.abs(b); j++) {
+   arr.push(0);
+ }
+} else {
+ let larger, smaller
+ if (Math.abs(a) >= Math.abs(b)) {
+   larger = a;
+   smaller = b;
+ } else {
+   larger = b;
+   smaller = a;
+ }
+
+ for (let k = 0; k < Math.abs(larger); k++) {
+   arr.push(0);
+ }
+
+ for (let l = 0; l < Math.abs(smaller); l++) {
+   arr.pop();
+ }
+}
+
+if (isNev) {
+ return -arr.length;
+} else {
+ return arr.length;
+}
+
 };
