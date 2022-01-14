48aec27fe2afc16dcf948d562410f67a61f25d72
Ximing Chen <steven.xm824@gmail.com>
2022-01-14 13:04:25 -0800

    simple solution

--- CountBits.js
+++ CountBits.js
@@ -1,7 +1,5 @@
 // Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
-
-
 // Example 1:
 
 // Input: n = 2
@@ -22,12 +20,9 @@
 // 4 --> 100
 // 5 --> 101
 
-
 // Constraints:
 
 // 0 <= n <= 105
-
-
 // Follow up:
 
 // It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
@@ -38,5 +33,12 @@
  * @return {number[]}
  */
  var countBits = function(n) {
-    //
+  const ans = [];
+
+  for (let i = 0; i <= n; i++) {
+    let count = i.toString(2).split('0').join('').length;
+    ans.push(count);
+  }
+
+  return ans;
 };
