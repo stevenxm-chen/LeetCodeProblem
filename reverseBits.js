d2cbd02dbeeb85ca72076cab6be49b5e886e8d46
Ximing Chen <steven.xm824@gmail.com>
2022-01-14 13:45:53 -0800

    solution

--- reverseBits.js
+++ reverseBits.js
@@ -1,11 +1,5 @@
 // Reverse bits of a given 32 bits unsigned integer.
 
-// Note:
-
-// Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
-// In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
-
-
 // Example 1:
 
 // Input: n = 00000010100101000001111010011100
@@ -27,5 +21,8 @@
  * @return {number} - a positive integer
  */
  var reverseBits = function(n) {
-    //
+  // padEnd function will add 0 to the end until reaching 32 digit
+  n = n.toString(2).splii('').reverse().join('').padEnd(32, '0');
+
+  return parseInt(n, 2);
 };
