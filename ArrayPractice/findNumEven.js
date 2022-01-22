d55ad6ee7602b9938a09903d6db53da7dcc47162
Ximing Chen <steven.xm824@gmail.com>
2022-01-21 16:24:29 -0800

    solution

--- ArrayPractice/findNumEven.js
+++ ArrayPractice/findNumEven.js
@@ -1,7 +1,5 @@
 // Given an array nums of integers, return how many of them contain an even number of digits.
 
-
-
 // Example 1:
 
 // Input: nums = [12,345,2,6,7896]
@@ -31,5 +29,15 @@
  * @return {number}
  */
  var findNumbers = function(nums) {
-    //
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      let curr = '' + nums[i];
+
+      if (curr.length % 2 === 0) {
+        count ++;
+      }
+    }
+
+    return count;
 };
