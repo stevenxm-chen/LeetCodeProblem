74b3e4e8f9a4bf0a071d4d31266a7359049948fc
Ximing Chen <steven.xm824@gmail.com>
2022-01-26 15:09:43 -0800

    dp solution

--- DecodeWay.js
+++ DecodeWay.js
@@ -37,5 +37,24 @@
 // s contains only digits and may contain leading zero(s).
 
 var numDecodings = function(s) {
-    //
+  if (s == null || s.length === 0) return 0;
+  if (s[0] === '0') return 0;
+
+  const dp = Array(s.length + 1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= s.length; i++) {
+
+    const a = Number(s[i - 1])
+    if (a >= 1 && a <= 9 ) {
+      dp[i] += dp[i - 1];
+    }
+    const b = Number(s[i - 2] + s[i - 1]);
+    if (b >= 10 && b <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+  return dp[s.length];
 };
