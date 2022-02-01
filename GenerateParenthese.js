b785c56b32fe26b25a2dd767b7903af6d050b0ba
Ximing Chen <steven.xm824@gmail.com>
2022-02-01 14:33:56 -0800

    working

--- GenerateParenthese.js
+++ GenerateParenthese.js
@@ -16,10 +16,34 @@
 
 // 1 <= n <= 8
 
-/**
+/*
  * @param {number} n
  * @return {string[]}
  */
- var generateParenthesis = function(n) {
 
+ var generateParenthesis = function(n) {
+  // set result
+  const result = [];
+
+  //use helper function to add ( an )
+  //open stands for counts of (  close for )
+
+  function helper (open, close, string) {
+    // base case
+    if(open === n && close === n) {
+      result.push(string)
+    }
+   //if open count < n then we need more pairs
+    if (open < n) {
+      helper (open + 1, close, string + '(')
+    }
+  // open and close should be same, so we need more to close
+    if (close < open) {
+      helper (open, close + 1, string + ')')
+    }
+  }
+  helper (0, 0, '');
+
+  //return result
+  return result
 };
