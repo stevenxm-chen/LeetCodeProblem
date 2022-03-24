e80488a3400fa8aeebc454c5fb7a38d1a1a1056c
Ximing Chen <steven.xm824@gmail.com>
2022-03-24 16:15:32 -0700

    solution

---
+++ tree/countTreenode.js
@@ -0,0 +1,40 @@
+// Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+// According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+// Design an algorithm that runs in less than O(n) time complexity.
+
+
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5,6]
+// Output: 6
+// Example 2:
+
+// Input: root = []
+// Output: 0
+// Example 3:
+
+// Input: root = [1]
+// Output: 1
+
+var countNodes = function(root) {
+  let count = 0;
+
+// use helper to traverse
+
+const postOrder = (tree) => {
+  // base case
+   if (!tree) return;
+  postOrder(tree.left);
+  postOrder(tree.right);
+
+  count++
+}
+
+postOrder(root)
+
+return count;
+};
